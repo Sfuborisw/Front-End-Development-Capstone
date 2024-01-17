d69afcf41ff5649db196d9d6a098dcad6fa61436
Sfuborisw <borix3739@gmail.com>
2024-01-17 03:53:33 -0800

    add test for submission

--- fedcapstone/src/test/BookingForm.test.js
+++ fedcapstone/src/test/BookingForm.test.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { useNavigate } from "react-router-dom";
 import BookingForm from "../components/BookingForm";
 
-describe("BookingForm2", () => {
+// Mock the useNavigate hook
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: jest.fn(),
+}));
+
+describe("BookingForm", () => {
   it("renders static text", () => {
     const availableTimes = ["18:00", "19:00", "20:00", "21:00", "22:00"];
 
     render(<BookingForm availableTimes={availableTimes} />);
+
+    expect(screen.getByLabelText("Choose date")).toBeInTheDocument();
+    expect(screen.getByLabelText("Choose time")).toBeInTheDocument();
+    expect(screen.getByLabelText("Number of guests")).toBeInTheDocument();
+    expect(screen.getByLabelText("Occasion")).toBeInTheDocument();
   });
+
   it("submits the form with specific values", () => {
     const dispatchMock = jest.fn();
+    const navigateMock = jest.fn();
+
+    useNavigate.mockReturnValue(navigateMock);
 
     render(<BookingForm availableTimes={[]} dispatch={dispatchMock} />);
 
@@ -36,5 +52,8 @@ describe("BookingForm2", () => {
     expect(screen.getByDisplayValue("2024-01-01")).toBeInTheDocument();
     expect(screen.getByDisplayValue("4")).toBeInTheDocument();
     expect(screen.getByDisplayValue("Birthday")).toBeInTheDocument();
+
+    // Check if the navigate function is called with the correct path
+    // expect(navigateMock).toHaveBeenCalledWith("/confirmed-booking");
   });
 });
